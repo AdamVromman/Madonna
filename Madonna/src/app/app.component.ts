417533e6fca16f2a461fa269fc533104aaeb6297
Vromman <adam.vromman@student.hogent.be>
2020-11-24 11:34:20 +0100

    kleiner logo, auto scrollen

--- Madonna/src/app/app.component.ts
+++ Madonna/src/app/app.component.ts
@@ -8,4 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Madonna';
   public isMenuCollapsed = true;
+
+  ngOnInit(): void {
+
+    let m = document.getElementById('m')
+    let adonna = document.getElementById('adonna')
+    let scrollEvent = document.getElementById('scrollEvent')
+
+
+    scrollEvent.addEventListener('mouseover', () =>{
+      if (document.body.scrollTop == 0)
+      {
+        scroll({left: 0, top: 200, behavior: 'smooth'})
+      }
+
+    })
+
+
+    // m.addEventListener('mouseenter', () => {
+    //  adonna.style.display = 'inline'
+    //  adonna.style.opacity = '1'
+    // })
+
+    // m.addEventListener('mouseleave', () => {
+    //   adonna.style.display = 'none'
+    //   adonna.style.opacity = '0'
+    // })
+
+  }
+
+  
+
+
 }
